941afba1bfb9df6d78e95b5987f02d8d6b312de9
Pedro Manse <pedro.manse@dmk3.com.br>
2025-07-01 19:28:43 -0300

    extracter interpreter from stck repo

---
+++ ci.sh
@@ -0,0 +1,29 @@
+#! /usr/bin/env bash
+set -e
+
+if [ -n "$FIX" ] && [ "$FIX" != "0" ] ; then
+	fix="--fix"
+fi
+
+if [ -n "$DIRTY" ] && [ "$DIRTY" != "0" ] ; then
+	allow_dirty="--allow-dirty"
+fi
+
+
+cargo build
+cargo fmt
+cargo clippy $fix $allow_dirty --all-targets --all-features -- \
+	-Dclippy::perf \
+	-Dclippy::style \
+	-Wclippy::pedantic \
+	-Aclippy::unnested_or_patterns \
+	-Aclippy::wildcard_imports \
+	-Aclippy::enum_glob_use \
+	-Aclippy::too_many_lines \
+	-Aclippy::match_same_arms \
+	-Aclippy::unnecessary_wraps \
+	-Aclippy::missing_errors_doc \
+	-Aclippy::cast_sign_loss \
+	-Aclippy::cast_possible_wrap \
+	-Aclippy::cast_possible_truncation
+cargo test
